51b55ed03cff706dcfa38cb25288ecfedb48d42d
niranjan reddy kyasaram <u2341654@uel.ac.uk>
2025-04-29 22:21:29 +0500

    Improve TxReceipt Component

--- packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
@@ -1,13 +1,18 @@
 import { TransactionReceipt } from "viem";
 import { displayTxResult } from "~~/components/scaffold-eth";
 
-export const TxReceipt = (
-  txResult: string | number | bigint | Record<string, any> | TransactionReceipt | undefined,
-) => {
+type TxReceiptProps = {
+  txResult: string | number | bigint | Record<string, any> | TransactionReceipt | undefined;
+};
+
+export const TxReceipt = ({ txResult }: TxReceiptProps) => {
   return (
     <div className="flex-wrap collapse collapse-arrow mb-2">
-      <input type="checkbox" className="min-h-0 peer" />
-      <div className="collapse-title text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-1.5">
+      <input type="checkbox" className="min-h-0 peer" id="tx-receipt-toggle" />
+      <div
+        className="collapse-title text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-1.5"
+        aria-labelledby="tx-receipt-toggle"
+      >
         <strong>Transaction Receipt</strong>
       </div>
       <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl">
