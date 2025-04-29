a4a2b2d11e2fb6d06b130660230c73b014ce759c
niranjan reddy kyasaram <u2341654@uel.ac.uk>
2025-04-29 22:07:36 +0500

    Refactor Transactions Table

--- packages/nextjs/components/blockexplorer/TransactionsTable.tsx
+++ packages/nextjs/components/blockexplorer/TransactionsTable.tsx
@@ -23,25 +23,29 @@ export const TransactionsTable = ({ blocks, transactionReceipts }: TransactionsT
             </tr>
           </thead>
           <tbody>
-            {blocks.map(block =>
-              (block.transactions as TransactionWithFunction[]).map(tx => {
+            {blocks.map((block) =>
+              (block.transactions as TransactionWithFunction[]).map((tx) => {
                 const receipt = transactionReceipts[tx.hash];
                 const timeMined = new Date(Number(block.timestamp) * 1000).toLocaleString();
                 const functionCalled = tx.input.substring(0, 10);
 
                 return (
-                  <tr key={tx.hash} className="hover text-sm">
+                  <tr key={tx.hash} className="hover:text-sm">
                     <td className="w-1/12 md:py-4">
                       <TransactionHash hash={tx.hash} />
                     </td>
                     <td className="w-2/12 md:py-4">
-                      {tx.functionName === "0x" ? "" : <span className="mr-1">{tx.functionName}</span>}
-                      {functionCalled !== "0x" && (
-                        <span className="badge badge-primary font-bold text-xs">{functionCalled}</span>
+                      {tx.functionName !== "0x" && (
+                        <>
+                          <span className="mr-1">{tx.functionName}</span>
+                          {functionCalled !== "0x" && (
+                            <span className="badge badge-primary font-bold text-xs">{functionCalled}</span>
+                          )}
+                        </>
                       )}
                     </td>
                     <td className="w-1/12 md:py-4">{block.number?.toString()}</td>
-                    <td className="w-2/1 md:py-4">{timeMined}</td>
+                    <td className="w-2/12 md:py-4">{timeMined}</td>
                     <td className="w-2/12 md:py-4">
                       <Address address={tx.from} size="sm" />
                     </td>
@@ -60,7 +64,7 @@ export const TransactionsTable = ({ blocks, transactionReceipts }: TransactionsT
                     </td>
                   </tr>
                 );
-              }),
+              })
             )}
           </tbody>
         </table>
