4d657f2a483d14866dd5bacd50b2a7dd5e9cda4e
niranjan reddy kyasaram <u2341654@uel.ac.uk>
2025-04-29 23:19:51 +0500

    Refactor Debug Page UI and Improve Contract Selection

--- packages/nextjs/pages/debug.tsx
+++ packages/nextjs/pages/debug.tsx
@@ -36,8 +36,8 @@ const Debug: NextPage = () => {
               <div className="flex flex-row gap-2 w-full max-w-7xl pb-1 px-6 lg:px-10 flex-wrap">
                 {contractNames.map(contractName => (
                   <button
-                    className={`btn btn-secondary btn-sm normal-case font-thin ${
-                      contractName === selectedContract ? "bg-base-300" : "bg-base-100"
+                    className={`btn btn-sm normal-case font-thin ${
+                      contractName === selectedContract ? "bg-base-300 text-white" : "bg-base-100 text-neutral"
                     }`}
                     key={contractName}
                     onClick={() => setSelectedContract(contractName)}
@@ -47,13 +47,7 @@ const Debug: NextPage = () => {
                 ))}
               </div>
             )}
-            {contractNames.map(contractName => (
-              <ContractUI
-                key={contractName}
-                contractName={contractName}
-                className={contractName === selectedContract ? "" : "hidden"}
-              />
-            ))}
+            <ContractUI contractName={selectedContract} />
           </>
         )}
       </div>
